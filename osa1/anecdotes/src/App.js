84a09e14d17ce280a717f038250ba68294208830
MariaNybackaKuronen <maria.n.1997@hotmail.com>
2021-12-08 19:36:11 +0200

    osa1

---
+++ osa1/anecdotes/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+// Math.floor(Math.random() * 10);
+
+const App = () => {
+
+  const randomNo = () => {
+  return Math.floor(Math.random() * 7);
+  }
+
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+  const handleClick = () => {
+    setSelected(randomNo())
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected]++
+    setVotes(copy)
+  }
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  console.log(votes)
+
+  const reducer = (prev, curr, i) =>{
+    if (curr > votes[prev]) return i
+    else return prev
+  }
+
+  const popular = votes.reduce(reducer, 0)
+
+  return (
+    <div>
+      <h1>
+        Anecdote of the day
+      </h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleClick}>next anecdote</button>
+      <h1>
+        Anecdote with most votes
+      </h1>
+      <p>{anecdotes[popular]}</p>
+      <p>has {votes[popular]} votes</p>
+    </div>
+  )
+}
+
+export default App
